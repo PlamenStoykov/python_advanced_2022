f32825e3af1d1f17315f178b9b74bda50ef8eea1
PlamenStoykov <102241770+PlamenStoykov@users.noreply.github.com>
2022-05-14 20:10:54 +0300

    Delete new directory

--- new/lists_as_stacks_and_queues/hot_potato.py
+++
@@ -1,8 +0,0 @@
-from collections import deque
-players = deque(input().split())
-
-n = int(input())
-for i in range(len(players)-1):
-    players.rotate(-n)
-    print(f"Removed {players.pop()}")
-print(f"Last is {players.pop()}")
--- new/lists_as_stacks_and_queues/matching_parentheses.py
+++
@@ -1,9 +0,0 @@
-expression = input()
-stack = []
-for i in range(len(expression)):
-    if expression[i] == '(':
-        stack.append(i)
-    elif expression[i] == ')':
-
-        open_bracket = stack.pop()
-        print(expression[open_bracket:i+1])
